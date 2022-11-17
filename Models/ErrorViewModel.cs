a6e7deb4bb8ad84c62364171b463834c7722ad5d
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-11-17 20:50:28 -0300

    MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AV1_TecProgram_Uninove.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
